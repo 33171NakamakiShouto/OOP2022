63079cad7fa6b33caa4c5fbf596e5b8b95a404b0
33171中牧翔斗 <33171@ojs.ac.jp>
2022-06-14 15:45:39 +0900

    統一試験完成（コメントあり）

--- Test202206/TestProject/Test01/ScoreCounter.cs
+++ Test202206/TestProject/Test01/ScoreCounter.cs
@@ -1,47 +1,41 @@
 ﻿using System.Collections.Generic;
 using System.IO;
 
-namespace Test01 {
-    class ScoreCounter {
+namespace Test01 
+{
+    class ScoreCounter 
+    {
         private IEnumerable<Student> _score;
 
         // コンストラクタ
-        public ScoreCounter(string filePath) {
-
+        public ScoreCounter(string filePath) 
+        {
             _score = ReadScore(filePath);
-
-
         }
 
 
-        //メソッドの概要： 
-        private static IEnumerable<Student> ReadScore(string filePath) {
-
-            List<Student> sales = new List<Student>();
+        //科目別の点数データを読み込み、Studentオブジェクトのリストを返す： 
+        private static IEnumerable<Student> ReadScore(string filePath) 
+        {
+            List<Student> scores = new List<Student>();
             string[] lines = File.ReadAllLines(filePath);
             foreach (string line in lines)
             {
                 string[] items = line.Split(',');
-                Student sale = new Student
+                Student score = new Student
                 {
                     Name = items[0],
                     Subject = items[1],
                     Score = int.Parse(items[2])
                 };
-                sales.Add(sale);
+                scores.Add(score);
             }
-            return sales;
-
-
-
-
-
-
+            return scores;
         }
 
-        //メソッドの概要： 
-        public IDictionary<string, int> GetPerStudentScore() {
-
+        //科目別の点数の合計を求める： 
+        public IDictionary<string, int> GetPerStudentScore() 
+        {
             var dict = new Dictionary<string, int>();
             foreach (var score in _score)
             {
@@ -51,10 +45,6 @@ namespace Test01 {
                     dict[score.Subject] = score.Score;
             }
             return dict;
-
-
-
-
         }
     }
 }
