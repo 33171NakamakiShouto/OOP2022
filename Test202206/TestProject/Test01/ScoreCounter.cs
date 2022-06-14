7ad3703a695baf1e3a64effe1a178e230c0ef584
33171中牧翔斗 <33171@ojs.ac.jp>
2022-06-14 15:26:10 +0900

    統一試験終了（コメントなし）

--- Test202206/TestProject/Test01/ScoreCounter.cs
+++ Test202206/TestProject/Test01/ScoreCounter.cs
@@ -8,7 +8,7 @@ namespace Test01 {
         // コンストラクタ
         public ScoreCounter(string filePath) {
 
-            
+            _score = ReadScore(filePath);
 
 
         }
@@ -16,24 +16,45 @@ namespace Test01 {
 
         //メソッドの概要： 
         private static IEnumerable<Student> ReadScore(string filePath) {
-            
+
+            List<Student> sales = new List<Student>();
+            string[] lines = File.ReadAllLines(filePath);
+            foreach (string line in lines)
+            {
+                string[] items = line.Split(',');
+                Student sale = new Student
+                {
+                    Name = items[0],
+                    Subject = items[1],
+                    Score = int.Parse(items[2])
+                };
+                sales.Add(sale);
+            }
+            return sales;
 
 
 
 
 
 
-            
         }
 
         //メソッドの概要： 
         public IDictionary<string, int> GetPerStudentScore() {
 
+            var dict = new Dictionary<string, int>();
+            foreach (var score in _score)
+            {
+                if (dict.ContainsKey(score.Subject))
+                    dict[score.Subject] += score.Score;
+                else
+                    dict[score.Subject] = score.Score;
+            }
+            return dict;
 
 
 
 
-            
         }
     }
 }
