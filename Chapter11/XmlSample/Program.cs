114084246352250eef60279973acbf19f38a2b37
33171中牧翔斗 <33171@ojs.ac.jp>
2022-06-24 15:57:49 +0900

    ニュース

---
+++ Chapter11/XmlSample/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+using System.Net;
+
+namespace XmlSample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var wc = new WebClient();
+            var stream = new WebClient().OpenRead("https://news.yahoo.co.jp/rss/media/abn/all.xml");
+
+            var xdoc = XDocument.Load(stream);
+            var xNews = xdoc.Root.Descendants("item");
+                //Elements().OrderBy(x => ((DateTime)x.Element("birth")).Year >= 1900);
+
+            foreach (var date in xNews)
+            {
+                /*var xname = xnovelist.Element("name");
+
+                var birth = (DateTime)xnovelist.Element("birth");
+                Console.WriteLine("{0} {1}",xname.Value,birth.ToShortDateString());*/
+                Console.WriteLine(date);
+            }
+        }
+    }
+}
