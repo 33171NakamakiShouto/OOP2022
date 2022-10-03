47d8af14d278744d58768391359da95a0147cbf9
33171中牧翔斗 <33171@ojs.ac.jp>
2022-10-03 15:58:18 +0900

    カテゴリできた

--- Chapter15/Chapter15/Program.cs
+++ Chapter15/Chapter15/Program.cs
@@ -10,13 +10,61 @@ namespace Chapter15
     {
         static void Main(string[] args)
         {
+            //var books = Library.Books
+            //    .OrderBy(b => b.CategoryId)
+            //    .ThenByDescending(b=>b.PublishedYear)
+            //    .ThenBy(b=>b.Price);
+            //foreach (var book in books)
+            //{
+            //    Console.WriteLine(book);
+            //}
+            //
+            Console.WriteLine("出力したい西暦を入力(終了:-1)");
+
+            var years = new List<int>();
+            var year = int.Parse(Console.ReadLine());
             var books = Library.Books
-                .OrderBy(b => b.CategoryId)
-                .ThenByDescending(b=>b.PublishedYear);
-            foreach (var book in books)
+                               .Where(b => years.Contains(b.PublishedYear));
+
+            while (!(year == -1))
             {
-                Console.WriteLine(book);
-            }            
+                years.Add(year);
+                year = int.Parse(Console.ReadLine());
+            }
+
+            Console.WriteLine();
+
+            Console.Write("昇順:1　降順:2 => ");
+
+            var order = int.Parse(Console.ReadLine());
+
+            if (order == 1)
+            {                                            
+                foreach (var book in books.OrderBy(b => b.PublishedYear))
+                {
+                    Console.WriteLine(book);
+                }                
+            }
+            else if (order == 2)
+            {
+
+                foreach (var book in books.OrderByDescending(b => b.PublishedYear))
+                {
+                    Console.WriteLine(book);
+                }
+            }
+
+            Console.WriteLine();
+
+            foreach (var g in books.GroupBy(b => b.PublishedYear).OrderBy(g => g.Key))
+            {
+                Console.WriteLine($"{g.Key}年");
+                foreach (var book in g)
+                {
+                    var category = Library.Categories.Where(b => b.Id == book.CategoryId).First();
+                    Console.WriteLine($"    タイトル:{book.Title},価格:{book.Price},カテゴリ:{category.Name}");
+                }
+            }
         }
     }
 }
