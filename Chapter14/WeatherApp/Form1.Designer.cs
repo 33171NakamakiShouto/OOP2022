deca097a3518a72aae77e07d8bca7d84b425deea
33171中牧翔斗 <33171@ojs.ac.jp>
2022-11-10 15:59:11 +0900

    とりあえず入れた

---
+++ Chapter14/WeatherApp/Form1.Designer.cs
@@ -0,0 +1,99 @@
+﻿
+namespace WeatherApp
+{
+    partial class btWeatherGet
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージド リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tbWeatherInfo = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.cbAreaCode = new System.Windows.Forms.ComboBox();
+            this.lbAreaCode = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // tbWeatherInfo
+            // 
+            this.tbWeatherInfo.Location = new System.Drawing.Point(79, 257);
+            this.tbWeatherInfo.Multiline = true;
+            this.tbWeatherInfo.Name = "tbWeatherInfo";
+            this.tbWeatherInfo.Size = new System.Drawing.Size(614, 181);
+            this.tbWeatherInfo.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("MS UI Gothic", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.button1.Location = new System.Drawing.Point(29, 38);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 35);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "取得";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // cbAreaCode
+            // 
+            this.cbAreaCode.FormattingEnabled = true;
+            this.cbAreaCode.Location = new System.Drawing.Point(471, 51);
+            this.cbAreaCode.Name = "cbAreaCode";
+            this.cbAreaCode.Size = new System.Drawing.Size(169, 20);
+            this.cbAreaCode.TabIndex = 2;
+            this.cbAreaCode.SelectedIndexChanged += new System.EventHandler(this.cbAreaCode_SelectedIndexChanged);
+            // 
+            // lbAreaCode
+            // 
+            this.lbAreaCode.FormattingEnabled = true;
+            this.lbAreaCode.ItemHeight = 12;
+            this.lbAreaCode.Location = new System.Drawing.Point(471, 77);
+            this.lbAreaCode.Name = "lbAreaCode";
+            this.lbAreaCode.Size = new System.Drawing.Size(169, 136);
+            this.lbAreaCode.TabIndex = 3;
+            // 
+            // btWeatherGet
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lbAreaCode);
+            this.Controls.Add(this.cbAreaCode);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.tbWeatherInfo);
+            this.Name = "btWeatherGet";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox tbWeatherInfo;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ComboBox cbAreaCode;
+        private System.Windows.Forms.ListBox lbAreaCode;
+    }
+}
+
---
+++ Chapter14/WeatherApp/Form1.cs
@@ -0,0 +1,144 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WeatherApp
+{
+    public partial class btWeatherGet : Form
+    {
+        public btWeatherGet()
+        {
+            InitializeComponent();
+            cbAreaCode.Items.Add("北海道地方");
+            cbAreaCode.Items.Add("東北地方");
+            cbAreaCode.Items.Add("関東甲信地方");
+            cbAreaCode.Items.Add("東海地方");
+            cbAreaCode.Items.Add("北陸地方");
+            cbAreaCode.Items.Add("近畿地方");
+            cbAreaCode.Items.Add("中国地方（山口県を除く）");
+            cbAreaCode.Items.Add("四国地方");
+            cbAreaCode.Items.Add("九州北部地方（山口県を含む）");
+            cbAreaCode.Items.Add("九州南部・奄美地方");
+            cbAreaCode.Items.Add("沖縄地方");
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            var wc = new WebClient()
+            {
+                Encoding = Encoding.UTF8
+            };
+
+            var dStrig = wc.DownloadString("https://www.jma.go.jp/bosai/forecast/data/overview_forecast/130000.json");
+            var json = JsonConvert.DeserializeObject<Rootobject>(dStrig);
+
+            tbWeatherInfo.Text = json.text;
+        }
+
+        private void cbAreaCode_SelectedIndexChanged(object sender, EventArgs e)
+        {          
+            switch (cbAreaCode.Text)
+            {
+                case "北海道地方":
+                    lbAreaCode.Items.Clear();
+                    lbAreaCode.Items.Add("宗谷地方");
+                    lbAreaCode.Items.Add("上川・留萌地方");
+                    lbAreaCode.Items.Add("網走・北見・紋別地方");
+                    lbAreaCode.Items.Add("十勝地方");
+                    lbAreaCode.Items.Add("釧路・根室地方");
+                    lbAreaCode.Items.Add("胆振・日高地方");
+                    lbAreaCode.Items.Add("石狩・空知・後志地方");
+                    lbAreaCode.Items.Add("渡島・檜山地方");
+                    break;
+                case "東北地方":
+                    lbAreaCode.Items.Clear();
+                    lbAreaCode.Items.Add("青森県");
+                    lbAreaCode.Items.Add("岩手県");
+                    lbAreaCode.Items.Add("宮城県");
+                    lbAreaCode.Items.Add("秋田県");
+                    lbAreaCode.Items.Add("山形県");
+                    lbAreaCode.Items.Add("福島県");
+                    break;
+                case "関東甲信地方":
+                    lbAreaCode.Items.Clear();
+                    lbAreaCode.Items.Add("茨城県");
+                    lbAreaCode.Items.Add("栃木県");
+                    lbAreaCode.Items.Add("群馬県");
+                    lbAreaCode.Items.Add("埼玉県");
+                    lbAreaCode.Items.Add("千葉県");
+                    lbAreaCode.Items.Add("東京都");
+                    lbAreaCode.Items.Add("神奈川県");
+                    lbAreaCode.Items.Add("山梨県");
+                    lbAreaCode.Items.Add("長野県");
+                    break;
+                case "東海地方":
+                    lbAreaCode.Items.Clear();
+                    lbAreaCode.Items.Add("岐阜県");
+                    lbAreaCode.Items.Add("静岡県");
+                    lbAreaCode.Items.Add("愛知県");
+                    lbAreaCode.Items.Add("三重県");
+                    break;
+                case "北陸地方":
+                    lbAreaCode.Items.Clear();
+                    lbAreaCode.Items.Add("新潟県");
+                    lbAreaCode.Items.Add("富山県");
+                    lbAreaCode.Items.Add("石川県");
+                    lbAreaCode.Items.Add("福井県");
+                    break;
+                case "近畿地方":
+                    lbAreaCode.Items.Clear();
+                    lbAreaCode.Items.Add("滋賀県");
+                    lbAreaCode.Items.Add("京都府");
+                    lbAreaCode.Items.Add("大阪府");
+                    lbAreaCode.Items.Add("兵庫県");
+                    lbAreaCode.Items.Add("奈良県");
+                    lbAreaCode.Items.Add("和歌山県");
+                    break;
+                case "中国地方（山口県を除く）":
+                    lbAreaCode.Items.Clear();
+                    lbAreaCode.Items.Add("鳥取県");
+                    lbAreaCode.Items.Add("島根県");
+                    lbAreaCode.Items.Add("岡山県");
+                    lbAreaCode.Items.Add("広島県");
+                    break;
+                case "四国地方":
+                    lbAreaCode.Items.Clear();
+                    lbAreaCode.Items.Add("徳島県");
+                    lbAreaCode.Items.Add("香川県");
+                    lbAreaCode.Items.Add("愛媛県");
+                    lbAreaCode.Items.Add("高知県");
+                    break;
+                case "九州北部地方（山口県を含む）":
+                    lbAreaCode.Items.Clear();
+                    lbAreaCode.Items.Add("山口県");
+                    lbAreaCode.Items.Add("福岡県");
+                    lbAreaCode.Items.Add("佐賀県");
+                    lbAreaCode.Items.Add("長崎県");
+                    lbAreaCode.Items.Add("熊本県");
+                    lbAreaCode.Items.Add("大分県");
+                    break;
+                case "九州南部・奄美地方":
+                    lbAreaCode.Items.Clear();
+                    lbAreaCode.Items.Add("宮崎県");
+                    lbAreaCode.Items.Add("奄美地方");
+                    lbAreaCode.Items.Add("鹿児島県（奄美地方除く）");
+                    break;
+                case "沖縄地方":
+                    lbAreaCode.Items.Clear();
+                    lbAreaCode.Items.Add("沖縄地方");
+                    lbAreaCode.Items.Add("大東島地方");
+                    lbAreaCode.Items.Add("宮古島地方");
+                    lbAreaCode.Items.Add("八重山地方");
+                    break;
+            }
+        }
+    }
+}
